8d0088b3f242014a5b8920dc852ab2bc7d8761bc
unknown <levasseurguillaume27@outlook.fr>
2020-11-27 12:03:25 +0100

    correction aqusition CSV + ajout coordo GPS

--- app/src/main/java/com/example/appliping/MainActivity.java
+++ app/src/main/java/com/example/appliping/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.appliping;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
@@ -30,30 +36,40 @@ public class MainActivity extends AppCompatActivity {
     //initialisation des variables components
     private EditText E_Text_Ip = null;
     private EditText E_Text_TimeOut = null;
-    private FileOutputStream Fos_filePing=null;
+    private EditText E_Text_Delay = null;
+    private Date D_dateNow = null;
+    private FileOutputStream Fos_filePing = null;
     private Button B_Start = null;
     private Button B_Stop = null;
     //initialisation des variables pour le timer
     private Timer T_timer = null;
     private TimerTask Tt_ping = null;
     Handler H_ping = new Handler();
+
+    private LocationManager LM_locationManager=null;
+    private String S_coordonees=null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //definition des variables aux components de l'apllication
         setContentView(R.layout.activity_main);
-        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},123);
-        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},123);
-        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.MANAGE_EXTERNAL_STORAGE},123);
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 123);
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 123);
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 123);
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.MANAGE_EXTERNAL_STORAGE}, 123);
 
+        LM_locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         E_Text_Ip = (EditText) findViewById(R.id.EditAdresseIP);
         E_Text_TimeOut = (EditText) findViewById(R.id.editTimeOut);
+        E_Text_Delay = (EditText) findViewById(R.id.editDelay);
         B_Start = (Button) findViewById(R.id.buttonStart);
         B_Stop = (Button) findViewById(R.id.buttonStop);
         //Ecouteur sur les button pour recuperate les event
         B_Start.setOnClickListener(EcouteurButton);
         B_Stop.setOnClickListener(EcouteurButton);
-        initFile();
+
+
     }
 
     /*
@@ -64,39 +80,46 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View v) {
 
-            if (TextUtils.isEmpty(E_Text_Ip.getText().toString())) { //Si le champ de text de l'adresse IP est vide
+            if (TextUtils.isEmpty(E_Text_Ip.getText().toString()) == true) { //Si le champ de text de l'adresse IP est vide
                 E_Text_Ip.setHintTextColor(Color.RED);  //afficher un placeholder rouge
                 E_Text_Ip.setHint("IP n'est pas saisi");
-            } else if (TextUtils.isEmpty(E_Text_TimeOut.getText().toString())) { //si le champ de text du timeOut est vide
+            } else if (TextUtils.isEmpty(E_Text_TimeOut.getText().toString()) == true) { //si le champ de text du timeOut est vide
                 E_Text_TimeOut.setHintTextColor(Color.RED); //Afficher un placeholder rouge
                 E_Text_TimeOut.setHint("TimeOut n'est pas saisi");
             } else { //Sinon on regarde l'état des bouton
-                if (B_Stop.isPressed()) {    //si le bouton stop est pressé on lance la fonction stopTimer
-                    StopTimer();
-                } else if (B_Start.isPressed()) {    //si le bouton start est pressé on lance la fonction startTimer
-                    startTimer();
+                if (B_Stop.isPressed() == true) {    //si le bouton stop est pressé on lance la fonction StopAcquisition
+                    StopAcquisition();
+                } else if (B_Start.isPressed() == true) {    //si le bouton start est pressé on lance la fonction StartAcquisition
+                    StartAcquisition();
                 }
             }
         }
 
     };
 
-    //  Fonction StartTimer
-    public void startTimer() {
+    //  Fonction StartAcquisition
+    public void StartAcquisition() {
         E_Text_Ip.setEnabled(false);
         E_Text_TimeOut.setEnabled(false);
         B_Start.setEnabled(false);
         B_Stop.setEnabled(true);
 
+        D_dateNow = new Date();
+        SimpleDateFormat Sdf_Formatdate = new SimpleDateFormat("YYYY_MM_dd_HH_mm_ss");
+        String S_FormatdateNow = Sdf_Formatdate.format(D_dateNow);
+        initFile(S_FormatdateNow);
+
+        GetCordonnees();
+
         T_timer = new Timer();            //initialisation du timer
         initializeTimerTask();          //initialisation du timerTask
-        T_timer.schedule(Tt_ping, 1000, 5000);
+        T_timer.schedule(Tt_ping, 1000, Integer.parseInt(E_Text_Delay.getText().toString()));
 
 
     }
 
-    //  Fonction StopTimer
-    public void StopTimer() {
+    //  Fonction StopAcquisition
+    public void StopAcquisition() {
         if (T_timer != null) {    //si le timer n'est pas null
             T_timer.cancel(); //annuler le timer
             T_timer = null;     //timer renitialised
@@ -112,6 +135,12 @@ public class MainActivity extends AppCompatActivity {
         E_Text_TimeOut.getText().clear();
         B_Start.setEnabled(true);
         B_Stop.setEnabled(false);
+
+        try {
+            Fos_filePing.close(); //Fermeture du fichier
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
@@ -129,7 +158,7 @@ public class MainActivity extends AppCompatActivity {
                             compteurPing++; //incrément du compteur
                             Log.i("début", "début du ping= " + compteurPing);     //écriture dans la console
                             PingResult pingResult = Ping.onAddress(E_Text_Ip.getText().toString()).setTimeOutMillis(Integer.parseInt(E_Text_TimeOut.getText().toString())).doPing(); //ping effécué a l'adresse IP et avec le timeOut
-                            if (pingResult.isReachable()) {     //Si le ping est effctué
+                            if (pingResult.isReachable() == true) {     //Si le ping est effctué
                                 Toast.makeText(MainActivity.this, "ping réussi ", Toast.LENGTH_SHORT).show();
                             } else {  //si le ping n'a pas abouti
                                 Toast toast = Toast.makeText(MainActivity.this, "connxion échoué , le timeOut est dépassé " + pingResult.getError(), Toast.LENGTH_SHORT);
@@ -146,56 +175,71 @@ public class MainActivity extends AppCompatActivity {
         };
     }
 
+    //Fonction qui retourne si la card SD est écrivable
     public boolean isExternalStorageWritable() {
         String state = Environment.getExternalStorageState();
-        if (Environment.MEDIA_MOUNTED.equals(state)) {
+        if (Environment.MEDIA_MOUNTED.equals(state) == true) {
             return true;
         }
         return false;
     }
 
+    //fonction qui retourne si la carte SD est lisable
     public boolean isExternalStorageReadable() {
         String state = Environment.getExternalStorageState();
-        if (Environment.MEDIA_MOUNTED.equals(state) ||
-                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+        if (Environment.MEDIA_MOUNTED.equals(state) == true ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state) == true) {
             return true;
         }
         return false;
     }
+
     //Fonction d'initialisation du fichier
-    public void initFile(){
+    public void initFile(String S_hordatage) {
         try {
-            String filename = "test.txt";
+            String filename = S_hordatage + "_test.txt";
             File file = new File(getExternalFilesDir(null), filename);
-            Fos_filePing=new FileOutputStream(file,true);
+            Fos_filePing = new FileOutputStream(file, true);
+            Fos_filePing.write(new String("date;temps_de_reponse;gps\n").getBytes());    //Ecriture des données
+
         } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     //Fonction qui écrit les donnée dans le fichier
     public void EcritureCSV(Float tempsPing) {
-            if (isExternalStorageWritable()) {
-                try {
-                    Date now = new Date();          //Date
-                    SimpleDateFormat formatDate = new SimpleDateFormat("dd/MM/YYYY HH:mm:ss");
-                    String date = formatDate.format(now);
-                    Fos_filePing.write(new String("date;temps_de_reponse;gps\n").getBytes());    //Ecriture des données
-                    Fos_filePing.write(new String(date + ";" + tempsPing + "ms\n").getBytes());
-                    Fos_filePing.close(); //Fermeture du fichier
-                } catch (FileNotFoundException e) {
-                    System.out.println(e);
-                } catch (IOException e) {
-                    System.out.println(e);
-                }
-            } else { //impossible d'ouvrir le fichier 
-                CharSequence text = "Votre carte SD n'est pas disponible à l'écriture !";
-                Toast.makeText(MainActivity.this,text,Toast.LENGTH_LONG).show();
-
-
+        if (isExternalStorageWritable() == true) {
+            try {
+                Date now = new Date();          //Date
+                SimpleDateFormat formatDate = new SimpleDateFormat("dd/MM/YYYY HH:mm:ss");
+                String date = formatDate.format(now);
+                Fos_filePing.write(new String(date + ";" + tempsPing + "ms"+";"+S_coordonees+"\n").getBytes());
+            } catch (IOException e) {
+                System.out.println(e);
             }
+        } else { //impossible d'ouvrir le fichier
+            CharSequence text = "Votre carte SD n'est pas disponible à l'écriture !";
+            AlertDialog.Builder ALertDialog_alertError_fichier = new AlertDialog.Builder(MainActivity.this);
+            ALertDialog_alertError_fichier.setTitle("erreur Fichier");
+            ALertDialog_alertError_fichier.setMessage(text);
+            ALertDialog_alertError_fichier.show();
+            StopAcquisition();
         }
     }
+
+    public void GetCordonnees(){
+       LocationProvider LP_fournisseur = LM_locationManager.getProvider("gps");
+        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+         Location L_localisation = LM_locationManager.getLastKnownLocation(LP_fournisseur.getName());
+             S_coordonees = String.format("L'altitude :%f -longitude : %f \n", L_localisation.getAltitude(), L_localisation.getLongitude());
+
+        }
+    }
+}
 
 
 
