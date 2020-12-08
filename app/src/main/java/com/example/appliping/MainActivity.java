ba473ee8ce3a1f910a2afa6c562cf3e37f20992c
unknown <levasseurguillaume27@outlook.fr>
2020-12-08 11:32:45 +0100

    commentaire anglais

--- app/src/main/java/com/example/appliping/MainActivity.java
+++ app/src/main/java/com/example/appliping/MainActivity.java
@@ -21,6 +21,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 import com.stealthcopter.networktools.Ping;
 import com.stealthcopter.networktools.ping.PingResult;
 import java.io.File;
@@ -35,72 +36,72 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
-    //initialisation des variables global
-    //initialisation des variables components
+    // init components variables
     private EditText E_Text_Ip = null;
     private EditText E_Text_TimeOut = null;
     private EditText E_Text_Delay = null;
     private Button B_Start = null;
     private Button B_Stop = null;
-    private Spinner S_listeAquisi=null;
-    private Date D_dateNow = null;
-    ArrayList<String>AL_listeAqui=null;
+    private Spinner S_list_Acquisition=null;
+    // init Global variable
+    private ArrayList<String>AL_list_Acquisition=null;
     private FileOutputStream Fos_filePing = null;
-    //initialisation des variables pour le timer
+    //init variables timer
     private Timer T_timer = null;
     private TimerTask Tt_ping = null;
-    Handler H_ping = new Handler();
+    private Handler H_ping = new Handler();
 
     private LocationManager LM_locationManager=null;
     private LocationListener LL_listenerGPS=null;
-    private String St_coordonees=null;
+    private String St_coordinate=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);  //Portrait orientation only
         setContentView(R.layout.activity_main);
-        //Demande a l'utilisateur la permission aux accès GPS et fichier
+        //Request at the user for permission to access GPS and storage
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.MANAGE_EXTERNAL_STORAGE}, 123);
 
-        LM_locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);  //recuperation du systeme de localisation de l'appareil
-        //definition des variables aux components de l'aplication
+        LM_locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);  //localisation system
+        //definition variables components
         E_Text_Ip = (EditText) findViewById(R.id.EditAdresseIP);
         E_Text_TimeOut = (EditText) findViewById(R.id.editTimeOut);
         E_Text_Delay = (EditText) findViewById(R.id.editDelay);
         B_Start = (Button) findViewById(R.id.buttonStart);
         B_Stop = (Button) findViewById(R.id.buttonStop);
-        S_listeAquisi=(Spinner)findViewById(R.id.spinner_resultAcqui);
+        S_list_Acquisition=(Spinner)findViewById(R.id.spinner_resultAcqui);
 
-        //Ecouteur sur les button pour recuperer les evenement
-        B_Start.setOnClickListener(EcouteurButton);
-        B_Stop.setOnClickListener(EcouteurButton);
+        //Listener on button
+        B_Start.setOnClickListener(ListenerButton);
+        B_Stop.setOnClickListener(ListenerButton);
 
 
     }
 
     /*
-    Fonction qui écoute l'état du button
+    Function Listener for the event
      */
-    public View.OnClickListener EcouteurButton = new View.OnClickListener() {
-        //Event click sur le boutton
+    public View.OnClickListener ListenerButton = new View.OnClickListener() {
+        //Event click
         @Override
         public void onClick(View v) {
-
-            if (TextUtils.isEmpty(E_Text_Ip.getText().toString()) == true) { //Si le champ de text de l'adresse IP est vide
-                E_Text_Ip.setHintTextColor(Color.RED);  //afficher un placeholder rouge
+            if (TextUtils.isEmpty(E_Text_Ip.getText().toString()) == true) { // if E_Text_ip is empty
+                E_Text_Ip.setHintTextColor(Color.RED);  //display placeholder red
                 E_Text_Ip.setHint("IP n'est pas saisi");
-            } else if (TextUtils.isEmpty(E_Text_TimeOut.getText().toString()) == true) { //si le champ de text du timeOut est vide
-                E_Text_TimeOut.setHintTextColor(Color.RED); //Afficher un placeholder rouge
+            } else if (TextUtils.isEmpty(E_Text_TimeOut.getText().toString()) == true) { //if E_Text_TimeOut is empty
+                E_Text_TimeOut.setHintTextColor(Color.RED); //display placeholder red
                 E_Text_TimeOut.setHint("TimeOut n'est pas saisi");
-            } else if(TextUtils.isEmpty(E_Text_Delay.getText().toString())==true){  //si le champ de texte du delay est vide
-                E_Text_Delay.setHintTextColor(Color.RED); //afficher un placeholder rouge
+            } else if(TextUtils.isEmpty(E_Text_Delay.getText().toString())==true){  //if E_Text_Delay is empty
+                E_Text_Delay.setHintTextColor(Color.RED); //display placeholder red
                 E_Text_Delay.setHint("Le delay n'est pas saisi");
             }
-            else { //Sinon on regarde l'état des bouton
-                if (B_Stop.isPressed() == true) {    //si le bouton stop est pressé on lance la fonction StopAcquisition
+            else if(LM_locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)==false)  //if GPS isn't active
+                Toast.makeText(MainActivity.this,"GPS is disabled",Toast.LENGTH_LONG).show();
+            else { //else listen state of button
+                if (B_Stop.isPressed() == true) {    //Function StopAcquisition
                     StopAcquisition();
-                } else if (B_Start.isPressed() == true) {    //si le bouton start est pressé on lance la fonction StartAcquisition
+                } else if (B_Start.isPressed() == true) {    //Function StartAcquisition
                     StartAcquisition();
                 }
             }
@@ -108,41 +109,41 @@ public class MainActivity extends AppCompatActivity {
 
     };
 
-    //  Fonction StartAcquisition
+    //  Function StartAcquisition
     public void StartAcquisition() {
         E_Text_Ip.setEnabled(false);
         E_Text_TimeOut.setEnabled(false);
         E_Text_Delay.setEnabled(false);
         B_Start.setEnabled(false);
         B_Stop.setEnabled(true);
-        AL_listeAqui=new ArrayList<>();
+        AL_list_Acquisition=new ArrayList<>();
 
-        D_dateNow = new Date();     //initialisation du format d'horodatage
-        SimpleDateFormat Sdf_Formatdate = new SimpleDateFormat("YYYY_MM_dd_HH_mm_ss");
-        String S_FormatdateNow = Sdf_Formatdate.format(D_dateNow);
-        initFile(S_FormatdateNow);
+        Date D_dateNow = new Date();     //init  format timestamp
+        SimpleDateFormat Sdf_Format_date = new SimpleDateFormat("YYYY_MM_dd_HH_mm_ss");
+        String S_Format_date_Now = Sdf_Format_date.format(D_dateNow);
+        initFile(S_Format_date_Now);
 
-        GetCordonnees();        //fonction qui récupere les données GPS
+        Get_coordinate();        //Function recuperate data GPS
 
-        T_timer = new Timer();            //initialisation du timer
-        initializeTimerTask();          //initialisation du timerTask
-        T_timer.schedule(Tt_ping, 3000, Integer.parseInt(E_Text_Delay.getText().toString()));
+        T_timer = new Timer();            //init timer
+        initializeTimerTask();          //init timerTask
+        T_timer.schedule(Tt_ping, 3000, Integer.parseInt(E_Text_Delay.getText().toString())); 
 
 
     }
 
-    //  Fonction StopAcquisition
+    //  Function StopAcquisition
     public void StopAcquisition() {
-        if (T_timer != null) {    //si le timer n'est pas null
-            T_timer.cancel(); //annuler le timer
-            T_timer = null;     //timer renitialised
+        if (T_timer != null) {   //if timer not null
+            T_timer.cancel();    //cancel timer
+            T_timer = null;      //timer reinitialised
         }
-        if (Tt_ping != null) {     //si le timerTask
-            Tt_ping.cancel();    //annuler timerTask
-            Tt_ping = null;        //timerTask reinitialised
+        if (Tt_ping != null) {   //if timerTask not null
+            Tt_ping.cancel();    //cancel timerTask
+            Tt_ping = null;      //timerTask reinitialised
         }
-        LM_locationManager.removeUpdates(LL_listenerGPS);
-        //Reinitialisation de la page
+        LM_locationManager.removeUpdates(LL_listenerGPS); //erase update GPS
+        //Reinitialisation
         E_Text_Ip.setEnabled(true);
         E_Text_TimeOut.setEnabled(true);
         E_Text_Delay.setEnabled(true);
@@ -154,36 +155,33 @@ public class MainActivity extends AppCompatActivity {
 
 
         try {
-            Fos_filePing.close(); //Fermeture du fichier
+            Fos_filePing.close(); //close  file
         } catch (IOException e) {
             e.printStackTrace();
         }
 
     }
 
-    // Fonction initilazeTimerTask
+    // Function initializeTimerTask
     public void initializeTimerTask() {
-        //Definition du timerTask
+        //Define  timerTask
         Tt_ping = new TimerTask() {
-            int compteurPing = 0; //compteur de ping
+            int compteurPing = 0; //counter number ping
 
             @Override
             public void run() {
                 H_ping.post(new Runnable() {
                     public void run() {
                         try {
-                            compteurPing++; //incrément du compteur
-                            Log.i("début", "début du ping= " + compteurPing);     //écriture dans la console
-                            PingResult pingResult = Ping.onAddress(E_Text_Ip.getText().toString()).setTimeOutMillis(Integer.parseInt(E_Text_TimeOut.getText().toString())).doPing(); //ping effécué a l'adresse IP et avec le timeOut
-                            if (pingResult.isReachable() == true) {     //Si le ping est effctué
-                               // Toast.makeText(MainActivity.this, "ping réussi ", Toast.LENGTH_SHORT).show();
-                                Log.d("ping", "Reussite du ping N°"+compteurPing);
+                            compteurPing++; //counter increment
+                            Log.d("Acquisition", "begin of ping N°" + compteurPing);     //write on the console
+                            PingResult pingResult = Ping.onAddress(E_Text_Ip.getText().toString()).setTimeOutMillis(Integer.parseInt(E_Text_TimeOut.getText().toString())).doPing(); //ping on IP address and the timeOut
+                            if (pingResult.isReachable() == true) {     //if ping success
+                                Log.d("Acquisition", "success ping N°"+compteurPing);  //write result ping on the console
                             } else {  //si le ping n'a pas abouti
-
-                                //Toast.makeText(MainActivity.this, "connxion échoué , le timeOut est dépassé " + pingResult.getError(), Toast.LENGTH_SHORT).show();
-                                Log.d("ping", "Echec du ping N°"+compteurPing);
+                                Log.d("Acquisition", "failed ping N°"+compteurPing);     //write result ping on the console
                             }
-                            Ecriture_acquisition(compteurPing,pingResult.getTimeTaken());
+                            Write_acquisition(compteurPing,pingResult.getTimeTaken());      //Call Function Write_acquisition
                         } catch (UnknownHostException e) {
                             e.printStackTrace();
                         }
@@ -194,7 +192,7 @@ public class MainActivity extends AppCompatActivity {
         };
     }
 
-    //Fonction qui retourne si la card SD est écrivable
+    //Function returns if the SD card is writable
     public boolean isExternalStorageWritable() {
         String state = Environment.getExternalStorageState();
         if (Environment.MEDIA_MOUNTED.equals(state) == true) {
@@ -203,7 +201,7 @@ public class MainActivity extends AppCompatActivity {
         return false;
     }
 
-    //fonction qui retourne si la carte SD est lisable
+    //function returns if the SD card is readable
     public boolean isExternalStorageReadable() {
         String state = Environment.getExternalStorageState();
         if (Environment.MEDIA_MOUNTED.equals(state) == true ||
@@ -213,14 +211,13 @@ public class MainActivity extends AppCompatActivity {
         return false;
     }
 
-    //Fonction d'initialisation du fichier
-    public void initFile(String S_hordatage) {
+    //Function init file
+    public void initFile(String S_timestamp) {
         try {
-            String filename = S_hordatage + "_test.txt";
+            String filename = S_timestamp + "_test.txt";        //name of the file
             File file = new File(getExternalFilesDir(null), filename);
-            Fos_filePing = new FileOutputStream(file, true);
-            Fos_filePing.write(new String("date;temps_de_reponse;gps\n").getBytes());    //Ecriture des données
-
+            Fos_filePing = new FileOutputStream(file, true);     //create a new file
+            Fos_filePing.write(new String("date;temps_de_reponse;gps\n").getBytes());    //Header of the file
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -228,47 +225,47 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    //Fonction qui écrit les donnée dans le fichier
-    public void Ecriture_acquisition(int nombPing,Float tempsPing) {
-        Date now = new Date();          //Date
-        SimpleDateFormat formatDate = new SimpleDateFormat("dd/MM/YYYY HH:mm:ss");
-        String date = formatDate.format(now);
+    //Function write data on the file
+    public void Write_acquisition(int I_numberPing,Float F_TimePing) {
+        Date D_dateNow = new Date();          //Date of the ping
+        SimpleDateFormat SDF_formatDate = new SimpleDateFormat("dd/MM/YYYY HH:mm:ss");
+        String date = SDF_formatDate.format(D_dateNow);
 
-        AL_listeAqui.add(nombPing+";"+date+";"+tempsPing+"ms;"+St_coordonees+"\n");
-        S_listeAquisi.setAdapter(new ArrayAdapter<String>(MainActivity.this,R.layout.support_simple_spinner_dropdown_item,AL_listeAqui));
+        AL_list_Acquisition.add(I_numberPing + ";" + date +";"+ F_TimePing + "ms;" + St_coordinate + "\n");  //write into the spinner
+        S_list_Acquisition.setAdapter(new ArrayAdapter<String>(MainActivity.this,R.layout.support_simple_spinner_dropdown_item,AL_list_Acquisition));
         
         if (isExternalStorageWritable() == true) {
             try {
-                Log.d("Aquisition", "Ecriture CSV en cours...");
+                Log.d("Acquisition", "Write file ...");
                 
-                Fos_filePing.write(new String(date + ";" + tempsPing + "ms"+";"+St_coordonees+"\n").getBytes());
+                Fos_filePing.write(new String(date + ";" + F_TimePing + "ms" + ";" + St_coordinate + "\n" ).getBytes());
             } catch (IOException e) {
                 System.out.println(e);
             }
-        } else { //impossible d'ouvrir le fichier
-            CharSequence text = "Votre carte SD n'est pas disponible à l'écriture !";
-            AlertDialog.Builder ALertDialog_alertError_fichier = new AlertDialog.Builder(MainActivity.this);
-            ALertDialog_alertError_fichier.setTitle("erreur Fichier");
-            ALertDialog_alertError_fichier.setMessage(text);
-            ALertDialog_alertError_fichier.show();
+        } else { //not possible to open the file
+            CharSequence CharS_textError = "Your SD card is not available for writing!";
+            AlertDialog.Builder AlertDialog_alertError_file = new AlertDialog.Builder(MainActivity.this);
+            AlertDialog_alertError_file.setTitle("error file");
+            AlertDialog_alertError_file.setMessage(CharS_textError);
+            AlertDialog_alertError_file.show();
             StopAcquisition();
         }
     }
     
-        //fonction qui recupere les données GPS actuel
-    public void GetCordonnees() {
-        Log.d("Aquisition", "Récupération des données GPS");
+        //function who return the GPS data
+    public void Get_coordinate() {
+        Log.d("Acquisition", "Recuperation GPS data");
         try {
             try {
                 LM_locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, LL_listenerGPS= new LocationListener()
                 {
                     @Override
-                    public void onLocationChanged(@NonNull Location location) {
-                         St_coordonees = String.format( location.getLatitude()+","+ location.getLongitude());
+                    public void onLocationChanged(@NonNull Location Lo_location) {
+                         St_coordinate = String.format( Lo_location.getLatitude()+","+ Lo_location.getLongitude());
                     }
 
                     @Override
-                    public void onProviderDisabled(@NonNull String provider) {
+                    public void onProviderDisabled(@NonNull String S_provider) {
                         StopAcquisition();
                     }
 
